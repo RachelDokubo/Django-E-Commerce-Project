eff488b34257757ef5d9cd3c4436f0d985cce2d7
SourLemon <rachelosaki22@gmail.com>
2024-07-20 15:29:21 -0700

    .env and .gitignore added

--- webcapz/settings.py
+++ webcapz/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ntr4vs+ti^&%7wra(do%ho4@%kgk50343j_4qgm6ff!nh9wdvv'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
